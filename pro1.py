94e4c6b791fa8914ac8b8dcebc654bf637d2abb1
kiana-moharrami <117479513+kiana-moharrami@users.noreply.github.com>
2023-02-17 16:17:54 +0330

    Add files via upload

---
+++ pro1.py
@@ -0,0 +1,138 @@
+# Is the variable x a valid Python expression?yes
+
+# Is x + 4 a valid Python expression?yes
+
+# What affect does the unary + operator have when applied to a numeric expression?One unit is added to it
+
+""" Sort the following binary operators in order of high to low precedence: +, -, *, //, /, %, =.
+# -**
+# -/	//	%	*
+# -+	-
+# -==	!=
+# -<	>	<=	>=
+# -=	-=	+=	*=	/=	//=	%=	**=
+"""
+
+""" q6  Indicate what each of the following Python statements would print.
+# x = 2
+# print("x") -->  x
+# print('x') -->  x
+# print(x) -->  2
+# print("x + 1") -->  x+1
+# print('x' + 1) -->  Error
+# print(x + 1) -->  3
+"""
+
+
+""" q7
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+print(i1 + i2)   7
+print(i1 / i2)   0.4
+print(i1 // i2)  0
+print(i2 / i1)   2.5
+print(i2 // i1)  2
+print(i1 * i3)   -6
+print(d1 + d2)   0.7
+print(d1 / d2)   0.4
+print(d2 / d1)   2.5
+print(d3 * d1)   -1.0
+print(d1 + i2)   7.0
+print(i1 / d2)   0.4
+print(d2 / i1)   2.5
+print(i2 / d1)   2.5
+print(i1/i2*d1)  0.8
+print(d1*i1/i2)  0.8
+print(d1/d2*i1)  0.8
+print(i1*d1/d2)  0.8
+print(i2/i1*d1)  5.0
+print(d1*i2/i1)  5.0
+print(d2/d1*i1)  5.0
+print(i1*d2/d1)  5.0
+"""
+
+# q8  What is printed by the following statement: it means comment
+
+
+""" q9
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+
+print(i1 + (i2 * i3)) - 13
+print(i1 * (i2 + i3))   4
+print(i1 / (i2 + i3))   1.0
+print(i1 // (i2 + i3))  1
+print(i1 / i2 + i3) - 2.6
+print(i1 // i2 + i3) - 3
+print(3 + 4 + 5 / 3)    8.666666666
+print(3 + 4 + 5 // 3)   8
+print((3 + 4 + 5) / 3)  4.0
+print((3 + 4 + 5) // 3)  4
+print(d1 + (d2 * d3)) - 0.5
+print(d1 + d2 * d3) - 0.5
+print(d1 / d2 - d3)     0.9
+print(d1 / (d2 - d3))   0.36363636
+print(d1 + d2 + d3 / 3)  6.833333333
+print((d1 + d2 + d3) / 3)  2.16666666665
+print(d1 + d2 + (d3 / 3)) 6.833333333
+print(3 * (d1 + d2) * (d1 - d3))  52.5
+"""
+
+# 10. What symbol signifies the beginning of a comment in Python?   To start coding in Python, there is no need to explain anything to start, we just set the desired variable
+
+# 11. How do Python comments end?    By writing the print code
+
+# 12. Which is better, too many comments or too few comments?   Many comments will increase the readability of the code
+
+# 13. What is the purpose of comments?    In order to increase the readability of the code
+
+# 14. Why is human readability such an important consideration? So that if someone else who is not familiar with our program and codes can understand by reading the comments
+
+
+"""
+15. What circumstances can cause each of the following run-time errors to arise?
+• NameError  :   Occurs when a variable cannot be found in the local or global scope.
+• ValueError  :   It is displayed when a function receives the argument of the correct type but with an inappropriate value.
+• ZeroDivisionError   :   It occurs when the second act of division or module operation is zero.
+• IndentationError    :When it comes to the index is an out -of -spectrum sequence
+• OverflowError   :    It happens when the result of an arithmetic operation is larger than it is displayed.
+• SyntaxError   :    It is occurred by the decomposition and when the syntax error occurs.
+"""
+
+# q 16
+# x
+# print(x)
+# w = int("2")
+# print(w)
+# E = "hello"+2
+# print(E)
+
+"""q17
+   (a) x += 1
+   (b) x /= 2
+   (c) x -= 1
+   x += y
+   (e) x -= (y + 7)
+   (f) x *=2
+   (g) number_of_closed_cases += 2*ncc
+   """
+
+
+"""q19
+r = 0
+PI = 3.14159
+# Formula for the area of a circle given its radius
+C = 2*PI*r
+# Get the radius from the user
+r = float(input("Please enter the circle's radius: "))
+# Print the circumference
+print("Circumference is", C)
+"""
