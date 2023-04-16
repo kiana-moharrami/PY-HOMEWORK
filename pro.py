3aca7ef7896f10b1a77956fc83d9e2d8a210bbbf
kiana-moharrami <117479513+kiana-moharrami@users.noreply.github.com>
2023-04-16 21:36:59 +0330

    Add files via upload

---
+++ pro.py
@@ -0,0 +1,76 @@
+"""
+1. What is the difference between a class and an object?
+In this way, program codes are created in the form of small entities called classes. Classes are nothing more than a means by which data and operations related to each other can be placed in a category and separated from other parts of the code. With the help of classes, the behavior and performance of each piece of code is known and does not interfere with other parts of the code. Creating a class means creating a new type in the program, which can create several objects or instances of that type.\
+ An object-oriented program is the result of the communication and interaction of various objects created in it.
+
+
+2. What are some other names for the term instance variable?
+shark
+
+
+3. What is another name for the term method?
+Functions belonging to an object and functional property
+
+
+4. What symbol associates an object with a method invocation?
+expand def--init--
+
+
+5. How does a method differ from a function?
+Methods are actually functions that are defined inside each class. Each Python class can contain three types of methods
+
+capitalize
+center
+count
+encode
+...
+
+6. What method from the string class returns a new string with no leading or trailing whitespace? length
+
+
+7. What function returns the length of its string argument?
+
+The first argument is a string object containing the full name of the desired file (address + name + extension) on the computer disk, and the second argument is a string with a specific value and specifies the mode in which this file should be opened; This argument is optional and defaults to 'r'. For example, 
+pay attention to the following codes that are equivalent to each other:
+
+
+8. What type of object does the open function return?
+
+9. What does the second parameter of the open function represent?
+10. Write a program that stores the first 100 integers to a text file named numbers.txt. Each number
+should appear on a line all by itself.
+
+
+
+def turtle():
+    while True:
+        import turtle
+        t = turtle.Turtle()
+        window = turtle.Screen()
+        t.forward(100)
+        t.left(120)
+        t.forward(100)
+        t.left(120)
+        t.forward(100)
+turtle()
+
+
+
+import.turtle
+:(for i in range(1,361
+(turtle.forward(1
+(turtle.right(
+
+
+
+15. Does Python permit a programmer to change one symbol in a string object? If so, how
+16. What would be the consequences if a turtle.Turtle object were immutable
+17. In the context of programming, what is garbage
+18. What is garbage collection, and how does it work in Python
+19. Consider the following code:
+a = "ABC"
+b = a
+c = b
+
+
+"""
